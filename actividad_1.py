6d72ee5ff195172d70b06a1bc78d938e9414c14b
Mon-MCS <montse_patito@hotmail.com>
2022-12-02 11:00:55 -0500

    Primera actividad

---
+++ actividad_1.py
@@ -0,0 +1,60 @@
+"""
+1. Crea una función que pida por pantalla un número al usuario y que controle 
+mediante excepciones lo siguiente:
+a. Sólo se podrá introducir números enteros.
+b. Si el número es mayor de 10 se lanza una excepción con raise la cual hayas 
+creado previamente mediante 'class Miexcepcion(Exception)
+"""
+
+# Se muestra la clase que se crea para mostrar la excepción
+class Miexcepcion(Exception):
+    "Aparece cuando el número entero introducido es mayor de 10"
+    pass
+
+
+def funcion_solicitar():
+    try:
+        numero = int(input("Introduzca un número entero y menor de 11: "))
+        # Se establece la condición que controla si el número introducido es mayor de 10, 
+        # para que se muestre el error creado (apartado b.)
+        if (numero>10):
+            raise Miexcepcion({"num":"El número entero introducido es mayor de 10"})
+    # Se controla que el usuario sólo introduzca números enteros (apartado a.)   
+    except ValueError:
+        print("El valor introducido no es un número entero")
+    # Se controla la excepción de que el número introducido sea mayor que 10
+    except Miexcepcion as e:
+        aux = e.args[0]
+        print(aux["num"])
+    # Se controla si se produce otra excepción
+    except Exception as e:
+        print("Ha ocurrido un error: ", e)
+
+funcion_solicitar()
+
+""" 
+2. Abre un fichero .txt y controla mediante excepciones las diferentes casuisticas para
+que el progama no termine de forma inesperada. Utiliza el finally para cerrar el fichero.
+"""
+# Para que no salte un error en el if del finally, si no se encuentra el archivo,
+# se iguala f a False
+f = False
+from io import UnsupportedOperation
+try:
+    f = open("leccion_1.txt", 'r', encoding='utf-8')
+    f.write("Hola\n")
+    print(f.readlines())
+# Se controla si no se localiza el archivo
+except FileNotFoundError:
+    print("Error archivo no encontrado")
+# Se controla si se produce un fallo en la descodificación
+except UnicodeDecodeError:
+    print("Error al descodificar el documento")
+except UnsupportedOperation:
+    print("Error al intentar realizar una operación no permitida")
+# Se controla si se produce otra excepción
+except Exception as e:
+    print("Ha ocurrido un error: ", e)
+finally:
+    if f:
+        f.close()
